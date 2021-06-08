1fb82b58d7c08ab39eb93a1695af93cfa8d596ca
Jacob Hightower <jah795@nau.edu>
2021-06-08 00:38:33 -0700

    added a super class w hero methods, battle system

--- heroes.py
+++ heroes.py
@@ -1,31 +1,63 @@
 import random
 
 # contains heroes available
+class Hero:
+    def __init__(self, name):
+        self.name = name
 
-class Knight:
+    def attack(self,power,crit_chance,accuracy):
+        '''
+        The hero's attack method. Takes a power(int),crit_chance(int), and accuracy(int)
+        variable to roll.
+        '''
+        attack = random.randint(1,power)
+        crit = random.randint(1,100)
+        attack_roll = random.randint(1,100)
+        if attack_roll <= accuracy:
+            if crit <= crit_chance:
+                attack = attack * 2
+                print("A critical hit!")
+        else:
+            attack = attack * .5
+            print("A glancing blow...")
+        return attack
+
+class Knight(Hero):
     def __init__(self, name):
         self.name = name
         self.level = 1
         self.xp = 0
+        self.power = 80
+        self.crit_chance = 2
+        self.accuracy = 80
         self.hp = random.randint(250,300)
 
-class Wizard:
+class Wizard(Hero):
     def __init__(self, name):
         self.name = name
         self.level = 1
         self.xp = 0
+        self.power = 40
+        self.crit_chance = 4
+        self.accuracy = 70
         self.hp = random.randint(50,100)
 
-class Rogue:
+class Rogue(Hero):
     def __init__(self, name):
         self.name = name
         self.level = 1
         self.xp = 0
+        self.power = 100
+        self.crit_chance = 20
+        self.accuracy = 95
         self.hp = random.randint(75,100)
 
-class Warrior:
+class Warrior(Hero):
     def __init__(self, name):
         self.name = name
         self.level = 1
         self.xp = 0
+        self.power = 100
+        self.crit_chance = 10
+        self.accuracy = 90
         self.hp = random.randint(150,200)
--- program.py
+++ program.py
@@ -15,6 +15,7 @@ def print_header():
 def game_loop():
 
     name = input('What is your name, brave adventurer? \n >>> ')
+    print()
     hero_choices = random.choice(['Wizard', 'Knight', 'Rogue', 'Warrior'])
     
     if hero_choices == 'Wizard':
@@ -27,16 +28,28 @@ def game_loop():
         hero = heroes.Warrior(name)
 
     print(f"You spawned in as a level {hero.level} {hero_choices} with {hero.hp} hp")
+    print()
 
     while True:
 
         spawned_creature = creatures.random_creature_picker()
 
-        print(f"As you strolled through the forest, a wild {spawned_creature.type} by the name of {spawned_creature.name} appeared.")
-
-        cmd = input('Do you [a]ttack, [r]un away, or [l]ook around? (Enter "x" to close the game.) \n >>> ').lower()
+        print(f"As you strolled through the forest, a wild {spawned_creature.type} by the name of {spawned_creature.name} appeared in front of you.")
+        print()
+        print()
+        cmd = input(f'Do you [a]ttack the {spawned_creature.type}, [r]un away, or [l]ook around for another creature? (Enter "x" to close the game.) \n >>> ').lower()
         if cmd == 'a':
-            print('chose violence')
+            creature_defeated = False
+            print()
+            while creature_defeated == False:
+                attack = hero.attack(hero.power,hero.crit_chance,hero.accuracy)
+                print(f"You attack the {spawned_creature.type} and do {attack} damage.")
+                spawned_creature.hp = spawned_creature.hp - attack
+                if spawned_creature.hp <= 0:
+                    print(f"You defeated the {spawned_creature.type} and gained {spawned_creature.xp} XP!")
+                    print()
+                    creature_defeated = True
+                    hero.xp = hero.xp + spawned_creature.xp
         elif cmd == 'r':
             print('chose chicken')
         elif cmd == 'l':
